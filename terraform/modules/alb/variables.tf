c4ef1a9f042c657345f1588e45db25dfc8555cbf
YacqubAdan <yacquubadan.1@gmail.com>
2025-02-15 22:53:11 +0000

    add: alb  module configuration

---
+++ terraform/modules/alb/variables.tf
@@ -0,0 +1,37 @@
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID for the target group"
+}
+
+variable "subnet_ids" {
+    description = "The IDs of the subnets"
+    type        = list(string)
+}
+
+# ALB specific
+
+variable "alb_name" {
+    type        = string
+    description = "The name of the ALB"
+}
+
+variable "target_group_name" {
+  type        = string
+  description = "Name of the target group"
+}
+
+
+variable "alb_sg_id" {
+  type        = string
+  description = "Security group ID attached to the ALB"
+}
+
+variable "target_port" {
+  type        = number
+  description = "Port for the target group"
+}
+
+variable "cert_arn" {
+    type        = string
+    description = "ARN of the SSL certificate"
+}
