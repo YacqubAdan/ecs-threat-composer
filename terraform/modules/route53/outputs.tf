4793a373063ca917b09b2c2d6faa2cff4592e0e2
YacqubAdan <yacquubadan.1@gmail.com>
2025-02-15 22:53:54 +0000

    add: route53 & acm module configuration

---
+++ terraform/modules/route53/outputs.tf
@@ -0,0 +1,15 @@
+output "ssl_cert_arn" {
+    description = "The arn of the ssl cert"
+    value       = aws_acm_certificate.ssl_cert.arn
+}
+
+output "route53_zone_id" {
+    description = "The id of the Route53 Zone"
+    value       = data.aws_route53_zone.dns_zone.zone_id
+}
+
+output "root_domain" {
+    description = "The root domain"
+    value       = var.root_domain
+
+}
